8af27c725a3c6c8b53e0ca911e959de4ee2ef650
Ricardo.bacarreza98@gmail.com <Ricardo.bacarreza98@gmail.com>
2024-02-07 17:10:39 +0000

    creation of finance day table

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,3 +1,5 @@
+{{config (materialized = 'table')}}
+
 SELECT
 date_date,
 COUNT(orders_id) AS nb_transactions,
@@ -8,3 +10,4 @@ ROUND(SUM(operational_margin), 2) AS operational_margin
 FROM {{ref("int_orders_operational")}}
 GROUP BY date_date
 ORDER BY date_date DESC
+
